d62b696372f2c142af829df88375af4dafe75a94
이종호 <jongholee@ebay.com>
2019-11-20 11:07:32 +0900

    Initial Commit

---
+++ mac-os/script/sync_intellij_keymap.sh
@@ -0,0 +1,5 @@
+# TODO fix path config
+
+#cp -r /Users/jongholee/Library/Preferences/IntelliJIdea*/keymaps/keymap_mac.xml /Users/jongholee/dev/workspace/configuration-file/Mac/intelliJ/keymaps/
+
+#cp -r /Users/jongholee/Library/Preferences/IntelliJIdea*/templates /Users/jongholee/dev/workspace/configuration-file/Mac/intelliJ/templates/
---
+++ windows/script/sync_window_settings.sh
@@ -0,0 +1,1 @@
+# TODO Window Settings Sync here
