854ad62670624b427fce4dd03a5bd11131c9ef78
jongholee <jhleed3@gmail.com>
2019-11-23 22:18:28 +0900

    Add script that upload to tistory

--- mac-os/script/upload_blog_mac.sh
+++ mac-os/script/upload_blog_mac.sh
@@ -1,1 +1,22 @@
 # tistory blog upload script
+
+
+dir_upload_blog=$DIR_BLOG
+
+echo "dir_blog : " $dir_upload_blog
+
+echo "uploading .."
+
+cd $dir_upload_blog
+unzip $dir_upload_blog/*.zip
+
+markdown-tistory write
+
+
+echo "clear all resources"
+
+rm $dir_upload_blog/*.zip
+rm $dir_upload_blog/*.md 
+rm $dir_upload_blog/*.png
+rm $dir_upload_blog/*.jpeg
+
